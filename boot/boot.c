63f833243176392b558b539e37dc3e0fe1f9bf8e
Ícaro <telesicaro04@gmail.com>
2026-09-05 12:08:32 -0300

    Update boot.c

--- boot/boot.c
+++ boot/boot.c
@@ -1,3 +1,11 @@
+/*
+ NexisLoader
+ Handcrafted x86 Kernel Bootloader
+ Independent project within the Nexis ecosystem
+ Copyright (c) 2026 icarotelesdasilva
+ Licensed under the MIT License
+*/
+
 extern void vga_print(char *str);
 extern void vga_clear(void);
 
